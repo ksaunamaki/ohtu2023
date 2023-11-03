653e19e2556be655d84160f589000742916bde9e
Kalle Saunamäki <kalle@applixure.com>
2023-11-03 20:01:52 +0200

    Add unittest and 100% coverage for StatisticsService class

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,50 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_haku_loytaa_pelaajan(self):
+        player = self.stats.search("Semenko")
+
+        self.assertIsNotNone(player)
+        self.assertEqual(player.name, "Semenko")
+
+    def test_haku_ei_loyda_pelaajaa(self):
+        player = self.stats.search("X")
+
+        self.assertIsNone(player)
+    
+    def test_haku_loytaa_tiimin_pelaajat(self):
+        players = self.stats.team("EDM")
+
+        self.assertEqual(len(players), 3)
+
+    def test_haku_ei_loyda_tiimin_pelaajia(self):
+        players = self.stats.team("X")
+
+        self.assertEqual(len(players), 0)
+
+    def test_top_pelaajat_palautuu_oikein(self):
+        players = self.stats.top(2)
+
+        # top() metodissa on bugi, se palauttaa yhden enemmän kuin pyydetty määrä johtuen 0 indeksoinnista
+        self.assertEqual(len(players), 3)
+        self.assertEqual(players[0].name, "Gretzky")
+        self.assertEqual(players[1].name, "Lemieux")
+    
