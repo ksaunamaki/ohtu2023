80b9127707147b06bc54d5966d080bd3dc399cf8
Kalle Saunamäki <kalle.saunamaki@helsinki.fi>
2023-11-18 11:46:42 +0200

    Week 3, task 3

--- viikko3/login-robot/src/services/user_service.py
+++ viikko3/login-robot/src/services/user_service.py
@@ -1,4 +1,5 @@
 from entities.user import User
+import re
 
 
 class UserInputError(Exception):
@@ -37,4 +38,15 @@ class UserService:
         if not username or not password:
             raise UserInputError("Username and password are required")
 
-        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if len(username) < 3:
+            raise UserInputError("Username must be at least 3 characters")
+        
+        if not re.match("^[a-z]+$", username):
+            raise UserInputError("Username must contain only letters a-z")
+        
+        if len(password) < 8:
+            raise UserInputError("Password must be at least 8 characters")
+        
+        if re.match("^[a-z]+$", password):
+            raise UserInputError("Password must contain some other characters besides letters a-z")
+        
