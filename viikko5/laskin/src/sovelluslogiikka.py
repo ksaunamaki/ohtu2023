d349960637a51fb2c080d5f3292b8f8fe4ff0e77
Kalle Saunamäki <kalle.saunamaki@helsinki.fi>
2023-11-27 19:52:08 +0200

    Week 5, task 5

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,45 +1,62 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
-
-    def miinus(self, arvo):
-        self.tulos = self.tulos - arvo
-
-    def plus(self, arvo):
-        self.tulos = self.tulos + arvo
-
-    def nollaa(self):
-        self.tulos = 0
+        self.edellinen_komento = None
 
     def aseta_arvo(self, arvo):
         self.tulos = arvo
+
+    def aseta_edellinen_komento(self, komento):
+        self.edellinen_komento = komento
 
 class Summa:
     def __init__(self, sovellus, lue_syote):
         self.sovellus = sovellus
         self.lue_syote = lue_syote
+        self.edellinen = 0
 
     def suorita(self):
-        self.sovellus.plus(self.lue_syote())
+        self.edellinen = self.sovellus.tulos
+
+        self.sovellus.aseta_arvo(self.sovellus.tulos + self.lue_syote())
+        self.sovellus.aseta_edellinen_komento(self)
+
+    def kumoa(self):
+        self.sovellus.aseta_arvo(self.edellinen)
 
 class Erotus:
     def __init__(self, sovellus, lue_syote):
         self.sovellus = sovellus
         self.lue_syote = lue_syote
+        self.edellinen = 0
 
     def suorita(self):
-        self.sovellus.miinus(self.lue_syote())
+        self.edellinen = self.sovellus.tulos
+
+        self.sovellus.aseta_arvo(self.sovellus.tulos - self.lue_syote())
+        self.sovellus.aseta_edellinen_komento(self)
+
+    def kumoa(self):
+        self.sovellus.aseta_arvo(self.edellinen)
 
 class Nollaus:
     def __init__(self, sovellus):
         self.sovellus = sovellus
+        self.edellinen = 0
 
     def suorita(self):
-        self.sovellus.nollaa()
+        self.edellinen = self.sovellus.tulos
+
+        self.sovellus.aseta_arvo(0)
+        self.sovellus.aseta_edellinen_komento(self)
+
+    def kumoa(self):
+        self.sovellus.aseta_arvo(self.edellinen)
 
 class Kumoa:
     def __init__(self, sovellus):
         self.sovellus = sovellus
 
     def suorita(self):
-        pass
+        if self.sovellus.edellinen_komento is not None:
+            self.sovellus.edellinen_komento.kumoa()
