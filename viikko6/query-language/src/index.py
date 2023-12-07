ab6bdd24a7ace18f84ec3d8130944c1c765f0e24
Kalle Saunamäki <kalle.saunamaki@helsinki.fi>
2023-12-07 16:19:45 +0200

    Week 6, task 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, Or, HasAtLeast, PlaysIn, All, Not, HasFewerThan
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
